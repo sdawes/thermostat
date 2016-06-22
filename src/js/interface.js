7e2e50a0e6e50c4f3cd6b03e58595f58133c81e6
Stephen Dawes <sdawes@outlook.com>
2016-06-22 15:37:09 +0100

    add jQuery functionality

--- src/js/interface.js
+++ src/js/interface.js
@@ -1,6 +1,48 @@
 $(document).ready(function(){
 
+// ================== SET TEMPERATURE =====================
+
   var thermostat = new Thermostat();
-  $('#temperature').text(thermostat.temperature);
+  updateTemperature();
+
+  $('#increaseTemperature').on('click', function(){ // event listener
+    thermostat.increaseTemperature(); // update model
+    updateTemperature();
+  });
+
+  $('#decreaseTemperature').on('click', function(){ // event listener
+    thermostat.decreaseTemperature(); // update model
+    updateTemperature();
+  });
+
+// ================== RESET =====================
+
+  $('#reset').on('click', function(){
+    thermostat.reset();
+    updateTemperature();
+  });
+
+// ================== PSM ON AND OFF =====================
+
+  $('#powerSavingModeOff').on('click', function (){
+    thermostat.switchPowerSavingModeOff();
+    $('#power-saving-status').text("OFF");
+  });
+
+  $('#powerSavingModeOn').on('click', function (){
+    thermostat.switchPowerSavingModeOn();
+    $('#power-saving-status').text("ON");
+  });
+
+// ================== HELPERS =====================
+
+  function updateTemperature(){
+    $('#temperature').text(thermostat.temperature);
+    $('#temperature').attr('class', thermostat.energyUsage());
+  };
+
+// ================== SET POWER USAGE COLOURS =====================
+
+
 
 });
