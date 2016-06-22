4a87a627f5c188043991016d5468f003346caa02
Stephen Dawes <sdawes@outlook.com>
2016-06-22 09:24:42 +0100

    add getter method for temp

---
+++ spec/thermostatSpec.js
@@ -0,0 +1,17 @@
+'use strict';
+
+describe('Thermostat', function(){
+
+  var thermostat;
+
+  beforeEach(function() {
+    thermostat = new Thermostat();
+  });
+
+  it ('has a starting temperature of 20 degrees', function(){
+    expect(thermostat.getCurrentTemperature()).toEqual(20);
+  });
+
+  
+
+});
