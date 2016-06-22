bb086e875e44b7d9fbfd1b59d0604db75b0b1ef3
Stephen Dawes <sdawes@outlook.com>
2016-06-22 17:41:11 +0100

    add API functionality

--- src/js/interface.js
+++ src/js/interface.js
@@ -41,16 +41,16 @@ $(document).ready(function() {
         $('#temperature').attr('class', thermostat.energyUsage());
     };
 
-    function displayWeather(city) {
-        var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
-        var token = '&appid=a3d9eb01d4de82b9b8d0849ef604dbed';
-        var units = '&units=metric';
-        $.get(url + token + units, function(data) {
-            $('#current-temperature').text(data.main.temp);
-        })
-    }
+    $('#current-city').change(function() {
+      var city = $('#current-city').val();
+      var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+      var token = '&appid=3cfa1cf4fde7836547442f43ea2013c7';
+      var units = '&units=metric';
+      $.get(url + token + units, function(data) {
+        $('#current-temperature').text(data.main.temp);
+      })
+    })
 
-    displayWeather('London');
 
 
 });
